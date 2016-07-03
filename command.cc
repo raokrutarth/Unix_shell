f02e2b6d97370346ec19c13c22fee2c15848ed34
Krutarth <raok@purdue.edu>
2016-07-03 18:39:43 -0400

    Commit Shell

--- command.cc
+++ command.cc
@@ -174,8 +174,8 @@ void Command::execute()
 		if ( strcmp(_simpleCommands[i]->_arguments[0], "cd") == 0 )
 		{
 			int cd_ret = chdir(_simpleCommands[i]->_arguments[1] );
-			if(cd_ret < 0)
-				fprintf(stderr, "cd to %s failed\n", _simpleCommands[i]->_arguments[1]);
+			/*if(cd_ret < 0)
+				fprintf(stderr, "cd to %s failed\n", _simpleCommands[i]->_arguments[1]); */
 		}				 
 		else // For every simple command fork a new process
 		{
