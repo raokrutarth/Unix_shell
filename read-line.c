0c9c8030acc27c9f9645942bf399eb46ca1e08d6
Krutarth <raok@purdue.edu>
2016-07-16 18:21:12 -0400

    Commit Shell

--- read-line.c
+++ read-line.c
@@ -18,7 +18,17 @@ void read_line_print_usage()
   char * usage = "\n"
     " ctrl-?       Print usage\n"
     " Backspace    Deletes last character\n"
-    " up arrow     See last command in the history\n";
+    " up arrow     See last command in the history\n"
+    "left arrow  key: Move the cursor to the left and allow insertion at " 
+      "that position. If the cursor is at the beginning of the line it does nothing.\n"
+    "right arrow key: Move the cursor to the right and allow insertion at that position. "
+      "If the cursor is at the end  of the line it does nothing.\n"
+    "delete key(ctrl-D): Removes the character at the cursor. The characters in the right "
+      "side are shifted to the left.\n"
+    "backspace (ctrl-H)key: Removes the character at the position before the cursor. "
+      "The characters in the right side are shifted to the left.\n"
+    "Home key (or ctrl-A): The cursor moves to the beginning of the line\n"
+    "End key (or ctrl-E): The cursor moves to the end of the line\n";
   write(1, usage, strlen(usage));
 }
 char * read_line() 
@@ -36,7 +46,8 @@ char * read_line()
     // Read one character in raw mode.
     char ch;
     read(0, &ch, 1);
-    if (ch>=32) {
+    if (ch>=32 && ch < 127) 
+    {
       // It is a printable character. display it back
       write(1,&ch,1);
       // If max number of character reached return.
@@ -59,9 +70,9 @@ char * read_line()
       line_buffer[0]=0;
       break;
     }
-    else if (ch == 8) 
+    else if (ch == 8 || ch==127) 
     {
-      // <backspace> was typed. Remove previous character read.
+      // <backspace> or ctrl-H was typed. Remove previous character read.
       // Go back one character
       ch = 8;
       write(1,&ch,1);
@@ -74,6 +85,19 @@ char * read_line()
       // Remove one character from buffer
       line_length--;
     }
+    else if(ch == 4)
+    {
+      /*ctrl-D): Removes the character at the cursor. 
+      The characters in the right side are shifted to the left. */
+    }
+    else if(ch == 1)
+    {
+      /*Home key (or ctrl-A): The cursor moves to the beginning of the line */
+    }
+    else if(ch==5)
+    {
+      /*End key (or ctrl-E): The cursor moves to the end of the line */
+    }
     else if (ch==27) 
     {
       // Esc. Read two chars more
@@ -81,9 +105,9 @@ char * read_line()
       char ch2;
       read(0, &ch1, 1);
       read(0, &ch2, 1);
-      if (ch1==91 && ch2==65) 
+      if (ch1==91 && ch2==65) // up
       {
-        // Up arrow. Print next line in history.
+        // Print next line in history.
         // Erase old line
         // Print backspaces
         int i = 0;
@@ -111,7 +135,35 @@ char * read_line()
         history_index=(history_index+1)%history_length;
         // echo line
         write(1, line_buffer, line_length);
-      }      
+      }
+      else if(ch1==91 && ch2==66) //down 
+      {
+        //Shows the next command in the history list
+      } 
+      else if(ch1==91 && ch2==67) //right 
+      {
+        /* Move the cursor to the right and allow insertion at 
+        that position. If the cursor is at the end  of the line it does nothing. */
+        
+      } 
+      else if(ch1==91 && ch2==68) //left 
+      {
+        /* Move the cursor to the left and allow insertion at that 
+        position. If the cursor is at the beginning of the line it does nothing. */
+        
+      }
+      else if(ch1==91 && ch2==52) // <end>
+      {
+        //read char because end is 27+91+52+126
+        /* end key */
+        
+      }
+      else if(ch1==91 && ch2==49) // <HOME>
+      {
+        //read char because home is 27+91+49+126
+        /* home key */
+        
+      }  
     }
   }
   // Add eol and null char at the end of string
